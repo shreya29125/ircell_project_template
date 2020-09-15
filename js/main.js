ea6d2cd135cbc747ed71df5fe8bb7e0edd3e3348
shreya29125 <55193116+shreya29125@users.noreply.github.com>
2020-09-15 20:11:24 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
     $(window).on('load', function () {
       $('#preloader-active').delay(450).fadeOut('slow');
       $('body').delay(450).css({
-        'overflow': 'visible'
+        'overflow-x': 'hidden'
       });
     });
 
