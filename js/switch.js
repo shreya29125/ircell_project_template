71c7e228dad666b841a6375df6f8b9e7901675e7
shreya29125 <55193116+shreya29125@users.noreply.github.com>
2020-09-15 19:57:22 +0530

    Add files via upload

---
+++ js/switch.js
@@ -0,0 +1,36 @@
+$(document).ready(function() { 
+	$(".blue").click(function(){
+      $("link").attr("href", "css/blue.css");
+      return false;
+   });
+   
+   $(".yellow").click(function(){
+      $("link").attr("href", "css/yellow.css");
+      return false;
+   });
+   
+   $(".green").click(function(){
+      $("link").attr("href", "css/green.css");
+      return false;
+   });
+
+   $(".red").click(function(){
+      $("link").attr("href", "css/red.css");
+      return false;
+   });
+
+   $(".black").click(function(){
+      $("link").attr("href", "css/black.css");
+      return false;
+   });
+
+   $(".sweet").click(function(){
+      $("link").attr("href", "css/sweet.css");
+      return false;
+   });
+
+   $(".orange").click(function(){
+      $("link").attr("href", "css/orange.css");
+      return false;
+   });
+});
